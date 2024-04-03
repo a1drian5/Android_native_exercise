4cc32ad6d0e2b87a7ebe3db1e18933add33d256e
a1drian5 <a1drian5@hotmail.com>
2024-04-03 17:34:16 -0600

    Funciona, los fragments ya estan, solo falta la logica para llamarlos

--- TMDBClient/app/src/main/java/com/anushka/tmdbclient/presentation/topRateMovie/TopRateMovieActivity.kt
+++ TMDBClient/app/src/main/java/com/anushka/tmdbclient/presentation/topRateMovie/TopRateMovieActivity.kt
@@ -1,25 +1,22 @@
-package com.anushka.tmdbclient.presentation.movie
+package com.anushka.tmdbclient.presentation.topRateMovie
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.anushka.tmdbclient.R
-import com.anushka.tmdbclient.databinding.ActivityMovieBinding
-import dagger.hilt.android.AndroidEntryPoint
+import com.anushka.tmdbclient.databinding.ActivityTopRateMovieBinding
 
-@AndroidEntryPoint
-class MovieActivity : AppCompatActivity() {
-
+class TopRateMovieActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private lateinit var binding: ActivityMovieBinding
+    private lateinit var binding: ActivityTopRateMovieBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMovieBinding.inflate(layoutInflater)
+        binding = ActivityTopRateMovieBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.topRateNavHostFragment) as NavHostFragment
         navController = navHostFragment.navController
     }
 
